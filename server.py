63770e42b68d0639e26e05f763e28ef0ffc6ded6
Ruslan Pashayev <pashayevrn@gmail.com>
2023-04-20 19:13:33 -0500

    initial commit

---
+++ server.py
@@ -0,0 +1,19 @@
+from flask import Flask, render_template, request, redirect
+app = Flask(__name__)  
+
+@app.route('/')         
+def index():
+    return render_template("index.html")
+
+@app.route('/checkout',methods=['POST'])         
+def checkout():
+    print(f'Charging {request.form["first_name"]} {request.form["last_name"]} for {int(request.form["strawberry"])+int(request.form["raspberry"])+int(request.form["apple"])+int(request.form["blackberry"])} fruits"')
+    return render_template('checkout.html')
+
+@app.route('/fruits')         
+def fruits():
+    return render_template("fruits.html")
+
+if __name__=="__main__":
+    app.run(debug=True)
+    
